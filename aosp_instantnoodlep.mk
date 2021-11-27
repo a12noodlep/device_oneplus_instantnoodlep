c3e7978b5c8f21b57884df0210e6a04aa8c67afd
SwapnilBunny <yenkule@gmail.com>
2021-11-27 17:34:28 +0000

    back to aosp

--- aosp_instantnoodlep.mk
+++ aosp_instantnoodlep.mk
@@ -21,13 +21,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from instantnoodlep device
 $(call inherit-product, device/oneplus/instantnoodlep/device.mk)
 
-# Inherit some common Octavi stuff.
-$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+# Inherit some common PixelBlasterOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1440
 
-PRODUCT_NAME := octavi_instantnoodlep
+PRODUCT_NAME := aosp_instantnoodlep
 PRODUCT_DEVICE := instantnoodlep
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
